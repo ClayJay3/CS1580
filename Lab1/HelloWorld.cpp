0a09d4a5fa49f553a1f94c2f7213c8d0461111f0
ClayJay3 <claytonraycowen@gmail.com>
2022-01-29 17:42:13 -0600

    Initial Commit

---
+++ Lab1/HelloWorld.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+    Programmer: Clayton Cowen
+    Student ID: 12578792
+    Section: 307
+    Date: 01-26-22
+    File HelloWorld.cpp
+    Purpose: Get everything setup and test C++ compiler.
+******************************************************************************/
+#include <iostream>
+
+using namespace std;
+///////////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************
+        Main method.
+******************************************************************************/
+int main()
+{
+    // Print text to console.
+    cout << "Hello, world!" << endl;
+    cout << "My name is Clayton!" << endl;
+
+    // Report program exit status.
+    return EXIT_SUCCESS;
+}
